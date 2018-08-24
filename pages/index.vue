df29731549dcde5a2c6c9e65d773cf7fc86f8e57
FBerthelot <florent.berthelot35@gmail.com>
2018-08-24 17:26:08 +0200

    feat: animate home

--- pages/index.vue
+++ pages/index.vue
@@ -77,48 +77,119 @@ export default {
 }
 </script>
 <style scoped>
-.winner {
-  height: 80vh;
-  width: 100vw;
-  background: url("/static/background.jpg") no-repeat bottom fixed;
-  background-size: cover;
-  color: white;
-  position: relative;
-}
+  .winner {
+    height: 80vh;
+    width: 100vw;
+    background: url('/static/background.jpg') no-repeat bottom fixed;
+    background-size: cover;
+    color: white;
+    position: relative;
+  }
 
-.top {
-  position: absolute;
-  top: 5vw;
-  left: 5vw;
-}
-h1 {
-  margin-bottom: 16px;
-  font-family: Lato;
-  font-size: 70px;
-  font-weight: 300;
-  line-height: 84px;
-}
-.top p {
-  font-family: Roboto;
-  font-size: 30px;
-  line-height: 39px;
-}
+  .top {
+    position: absolute;
+    top: 5vh;
+    left: 5vw;
+    animation: 1s 1 titleapear;
+  }
+      @keyframes titleapear {
+        from {
+          left: -100vw
+        }
 
-.winnerAgency {
-  display: flex;
-  position: absolute;
-  width: 50vw;
-  bottom: 2vh;
-  right: 5vw;
-  justify-content: space-between;
-  align-items: flex-end;
-}
-.winnerAgency-desc {
-  text-align: right;
-  font-family: Roboto;
-  font-size: 22px;
-  line-height: 29px;
-}
+        to {
+          left: 5vw;
+        }
+      }
+
+    h1 {
+      margin-bottom: 16px;
+      font-family: Lato;
+      font-size: 70px;
+      font-weight: 300;
+      line-height: 84px;
+    }
+    .top p {
+      font-family: Roboto;
+      font-size: 30px;
+      line-height: 39px;
+    }
+
+  .winnerAgency {
+    display: flex;
+    position: absolute;
+    width: 50vw;
+    bottom: 2vh;
+    right: 5vw;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+    .winnerAgency-desc {
+      text-align: right;
+      font-family: Roboto;
+      font-size: 22px;
+      line-height: 29px;
+      animation: 1.5s 1 winnerAgency-desc-apear;
+    }
+      @keyframes winnerAgency-desc-apear {
+        0% {
+          transform: translateX(100vw);
+        }
+        50% {
+          transform: translateX(100vw);
+        }
+        100% {
+          transform: translateX(0);
+        }
+      }
+
+    .winnerAgency-card {
+      background-color: white;
+      color: black;
+      width: 16vw;
+      height: 22vh;
+      position: relative;
+      animation: 3s 1 bounceInDown cubic-bezier(0.1, -0.6, 0.2, 0);
+    }
+      @keyframes bounceInDown {
+        from,
+        60%,
+        75%,
+        90%,
+        to {
+          animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+        }
+
+        0% {
+          opacity: 0;
+          transform: translate3d(0, -3000px, 0);
+        }
+
+        60% {
+          opacity: 1;
+          transform: translate3d(0, 25px, 0);
+        }
+
+        75% {
+          transform: translate3d(0, -10px, 0);
+        }
+
+        90% {
+          transform: translate3d(0, 5px, 0);
+        }
+
+        to {
+          transform: translate3d(0, 0, 0);
+        }
+      }
+
+      .winnerAgency-card img.winnerAgency-card-king {
+        position: absolute;
+        top: -50px;
+        right: -46px;
+        width: 93px;
+        height: 75px;
+      }
 
 .winnerAgency-card {
   background-color: white;
@@ -171,41 +242,59 @@ h1 {
   height: 100%;
 }
 
-.agencies {
-  display: flex;
-  width: 100vw;
-  height: 20vh;
-  background-color: #000528;
-  justify-content: space-around;
-  align-items: center;
+  .agencies {
+    display: flex;
+    width: 100vw;
+    height: 20vh;
+    background-color: #000528;
+    justify-content: space-around;
+    align-items: center;
+    animation: 2s 1 fadeIn;
+  }
+
+  @keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  75% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
-.agencies a {
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.agencies a:hover {
-  transform: scale(1.1);
-}
-.agencies h2 {
-  font-family: Lato;
-  font-size: 20px;
-  font-weight: 300;
-  line-height: 24px;
-  text-align: center;
-}
-.agencies p {
-  font-family: Lato;
-  font-size: 50px;
-  font-weight: 300;
-  line-height: 60px;
-  text-align: center;
-}
-.agencies img {
-  width: 25%;
-}
+
+    .agencies a {
+      text-decoration: none;
+      color: white;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      transition: all 0.2s ease-in-out;
+    }
+
+    .agencies a:hover {
+      transform: scale(1.1);
+    }
+
+    .agencies h2 {
+      font-family: Lato;
+      font-size: 20px;
+      font-weight: 300;
+      line-height: 24px;
+      text-align: center;
+    }
+    .agencies p {
+      font-family: Lato;
+      font-size: 50px;
+      font-weight: 300;
+      line-height: 60px;
+      text-align: center;
+    }
+    .agencies img {
+      width: 25%;
+    }
 </style>
