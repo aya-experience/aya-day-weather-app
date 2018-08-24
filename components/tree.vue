40f40df5d4ae39fb71d16c6f4bde3d9ae76f5fbf
l-bonanni <laetitia.bonanni@zenika.com>
2018-08-24 16:08:35 +0200

    agency default template

---
+++ components/tree.vue
@@ -0,0 +1,41 @@
+<template>
+    <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 315 353.8" style="enable-background:new 0 0 315 250;" xml:space="preserve">
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M124.5,276.5c0,0,21.5-42.5,7-102s-80-24.5-103-77"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M136.5,276.5c0,0,22-108-17.5-155s-97.5-65-97.5-65"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M147,276.5c0,0,14.5-158.5-31.5-228c0,0-27,6-39.5-8.5"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M157.5,276.5c0,0-8-174,0-191.5c0,0-19.8-19.5-14.9-36.5"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M165.5,276.5c0,0-22.5-153,46-182.5c0,0,19-25.8,2.5-50.9"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M175,276.5c0,0-23.5-108,17.5-130.5s57.5,20.5,57.5,20.5"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M239,150c0,0,13-2,19-10.5"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M209.1,139.5c0,0,19.4-8.6,29.9-27.1"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M89,134.9c0,0-24.7,0.1-38.6,16.6"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M61.6,127.3c0,0-2.9-21.3,0-29.8"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M178.8,127.3c0,0,8.8-32.8,0-39.3"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M96.9,99.4c0,0-12.9-0.6-16.9,13"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M119,121.5c0,0-13.8-31-12.6-38.8"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M64.7,76.6c0,0-13.7-33.1-6.7-47.6"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M211.5,94c0,0,47-8,48.5-39.2"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M187,24.5c-26.5,8-29.5,60.5-29.5,60.5s23-27,35-23.3"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M123,61.7c0,0-0.3-30.7,19.6-37.2"/>
+      <path style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" d="
+          M115.5,48.5c0,0-12.5-13.5-12.5-33"/>
+    </svg>
+</template>
--- pages/agencies/_agency.vue
+++ pages/agencies/_agency.vue
@@ -1,11 +1,38 @@
 <template>
   <section>
-    {{agency.name}}
+    <div class="top">
+      <span>19°</span>
+      <nav>
+        <div>
+          <a href="#"><leftArrow /></a>
+          <a href="#"><rightArrow /></a>
+        </div>
+      </nav>
+      <span>35°</span>
+    </div>
+    <h1>{{agency.name}}</h1>
+    <h2>35°</h2>
+
+    <div class="tree">
+      <tree />
+    </div>
+
+
+    <a class="back-to-home" href="#">Back to home</a>
   </section>
 </template>
 
 <script>
+import tree from '../../components/tree.vue';
+import leftArrow from '../../components/left_arrow.vue';
+import rightArrow from '../../components/right_arrow.vue';
+
 export default {
+  components: {
+    tree,
+    leftArrow,
+    rightArrow
+  },
   computed: {
     agency() {
       return this.$store.state.agencies.find(agency => agency.name === this.$route.params.agency);
@@ -15,5 +42,82 @@ export default {
 </script>
 
 <style scoped>
+.back-to-home {
+  position: absolute;
+  bottom: 2vw;
+  left: 2vw;
 
+  color: black;
+  text-decoration: none;
+  font-family: "Lato", sans-serif;
+  font-size: 20px;
+  font-weight: 300;
+  line-height: 24px;
+}
+
+.back-to-home:before {
+  content: "<";
+}
+
+.top {
+  padding: 2vw 2vh 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.top span {
+  font-family: "Lato", sans-serif;
+  font-size: 3vw;
+  font-weight: 300;
+}
+
+nav {
+  flex: 1;
+  text-align: center;
+}
+
+nav a {
+  display: inline-block;
+  height: 8vh;
+}
+
+nav svg {
+  display: block;
+  height: 100%;
+  width: auto;
+}
+
+nav a + a {
+  margin-left: 25px;
+}
+
+h1 {
+  margin-top: 6vh;
+  font-family: "Lato", sans-serif;
+  font-size: 4vw;
+  font-weight: 300;
+  text-align: center;
+}
+
+h2 {
+  font-family: "Lato", sans-serif;
+  font-size: 8vw;
+  font-weight: 300;
+  margin-top: 2vh;
+  text-align: center;
+}
+
+.tree {
+  height: 50%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+
+.tree svg {
+  width: 100%;
+  height: 100%;
+}
 </style>
