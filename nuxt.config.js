adb6fe15d353efb721507d2a9adbf9c0d12f33f5
Christopher Kade <c.kade96@gmail.com>
2018-10-09 14:59:16 +0200

    Changed project desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Compétition météo entre les agences Zenika',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
